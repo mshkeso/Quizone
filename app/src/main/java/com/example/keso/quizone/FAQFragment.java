1360239e893f1849015a7391626c9cada156a8ed
Mshkeso <msh.keso@gmail.com>
2017-02-20 10:47:57 +0100

    Fix för felrapport 4

--- app/src/main/java/com/example/keso/quizone/FAQFragment.java
+++ app/src/main/java/com/example/keso/quizone/FAQFragment.java
@@ -1,10 +1,13 @@
 package com.example.keso.quizone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by KESO on 20/02/2017.
@@ -12,10 +15,28 @@ import android.view.ViewGroup;
 
 public class FAQFragment extends Fragment {
 
+    Button mail;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_faq, container, false);
+        mail = (Button) v.findViewById(R.id.btnMail);
+        mail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"exemple@example.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "");
+                i.putExtra(Intent.EXTRA_TEXT   , "");
+                try {
+                    startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(getActivity(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         return v;
     }
 
